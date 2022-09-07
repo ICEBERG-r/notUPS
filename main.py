e991186925414aeaac0feb58e93fc31f93c89f20
Mark Wilson <69532317+wilsonERR@users.noreply.github.com>
2022-09-07 09:31:23 -0500

    basic hash table and package class created

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-# This is a sample Python script.
+# Mark Wilson
+# Student ID:  010314264
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class HashHashBaby:
+    def __init__(self, initial_capacity=20):
+        self.table = []
+        for i in range(initial_capacity):
+            self.table.append([])
+
+    def insert(self, item):
+        bucket = hash(item) % len(self.table)
+        bucket_list = self.table[bucket]
+
+        bucket_list.append(item)
+
+    def search(self, key):
+        bucket = hash(key) % len(self.table)
+        bucket_list = self.table[bucket]
+
+        if key in bucket_list:
+            item_index = bucket_list.index(key)
+            return bucket_list[item_index]
+        else:
+            print('Item is not found')
+            return None
+
+    def remove(self, key):
+        bucket = hash(key) % len(self.table)
+        bucket_list = self.table[bucket]
+
+        if key in bucket_list:
+            bucket_list.remove(key)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Package:
+    def __init__(self, package_id, address, city, state, zip_code, deadline, weight, status):
+        self.package_id = package_id
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip_code = zip_code
+        self.deadline = deadline
+        self.weight = weight
+        self.status = status
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+myHash = HashHashBaby()
+myHash.insert('Hello World')
+print(myHash.table)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(myHash.search('Hello World'))
