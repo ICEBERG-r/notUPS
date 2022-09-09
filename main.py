403f667abe72d9c2f4f7570c8c38582d376695dd
Mark Wilson <69532317+wilsonERR@users.noreply.github.com>
2022-09-08 22:34:51 -0500

    added loader for packages.csv

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # Mark Wilson
 # Student ID:  010314264
+import csv
+
 
 class HashHashBaby:
     def __init__(self, initial_capacity=20):
@@ -43,9 +45,30 @@ class Package:
         self.weight = weight
         self.status = status
 
+    def __str__(self):
+        return "%s, %s, %s, %s, %s, %s, %s, %s" % (self.package_id, self.address, self.city, self.state,
+                                                   self.zip_code, self.deadline, self.weight, self.status)
+
+
+def load_packages(filename):
+    with open(filename) as packages:
+        package_data = csv.reader(packages, delimiter=',')
+        next(package_data)
+        for package in package_data:
+            p_id = int(package[0])
+            p_address = package[1]
+            p_city = package[2]
+            p_state = package[3]
+            p_zip = package[4]
+            p_deadline = package[5]
+            p_weight = package[6]
+            p_status = "Loaded"
+
+            package = Package(p_id, p_address, p_city, p_state, p_zip, p_deadline, p_weight, p_status)
+
+            myHash.insert(p_id, package)
+
 
 myHash = HashHashBaby()
-myHash.insert('Hello World')
-print(myHash.table)
 
-print(myHash.search('Hello World'))
+load_packages('packages.csv')
