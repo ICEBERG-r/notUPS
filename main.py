b3dcd24781b0b7c85d1e2148804cffd9f61245f9
Mark Wilson <69532317+wilsonERR@users.noreply.github.com>
2022-09-23 07:36:56 -0500

    updated preliminary package loading roster for each truck fixed hash added notes section to csv added notes field to Package class added Truck and Driver classes

--- main.py
+++ main.py
@@ -3,8 +3,8 @@
 import csv
 
 
-class HashHashBaby:
-    def __init__(self, initial_capacity=64):
+class HashMap:
+    def __init__(self, initial_capacity=10):
         self.table = []
         for i in range(initial_capacity):
             self.table.append([])
@@ -26,10 +26,10 @@ class HashHashBaby:
         bucket = hash(key) % len(self.table)
         bucket_list = self.table[bucket]
 
-        # for kv in bucket_list:
-        #   if kv[0] == key:
-        #      return kv[1]
-        # return None
+        for kv in bucket_list:
+            if kv[0] == key:
+                return kv[1]
+        return None
 
     def remove(self, key):
         bucket = hash(key) % len(self.table)
@@ -41,7 +41,7 @@ class HashHashBaby:
 
 
 class Package:
-    def __init__(self, package_id, address, city, state, zip_code, deadline, weight, status):
+    def __init__(self, package_id, address, city, state, zip_code, deadline, weight, notes, status):
         self.package_id = package_id
         self.address = address
         self.city = city
@@ -49,6 +49,7 @@ class Package:
         self.zip_code = zip_code
         self.deadline = deadline
         self.weight = weight
+        self.notes = notes
         self.status = status
 
     def __str__(self):
@@ -56,27 +57,50 @@ class Package:
                                                    self.zip_code, self.deadline, self.weight, self.status)
 
 
+class Truck:
+
+    def __init__(self):
+        self.payload = dict()
+        self.speed = 18
+
+    def load(self):
+        pass
+
+    def unload(self):
+        pass
+
+    def set_driver(self, driver):
+        pass
+
+
+class Driver:
+
+    def __init__(self):
+        self.isDriving = False
+
+
 def load_packages(filename):
-    with open(filename) as packages:
+    with open(filename, encoding="utf-8") as packages:
         package_data = csv.reader(packages, delimiter=',')
+        next(package_data)
         for package in package_data:
-            p_id = package[0]
+            p_id = int(package[0])
             p_address = package[1]
             p_city = package[2]
             p_state = package[3]
             p_zip = package[4]
             p_deadline = package[5]
             p_weight = package[6]
-            p_status = "Loaded"
+            p_notes = package[7]
+            p_status = "At Hub"
 
-            p = Package(p_id, p_address, p_city, p_state, p_zip, p_deadline, p_weight, p_status)
+            p = Package(p_id, p_address, p_city, p_state, p_zip, p_deadline, p_weight, p_notes, p_status)
 
-            myHash.insert(p_id, p)
+            packageHash.insert(p_id, p)
 
 
-myHash = HashHashBaby()
+packageHash = HashMap()
 
 load_packages('packages.csv')
 
-for k in range(len(myHash.table)):
-    print("Package: {}".format(myHash.search(k)))
+print(packageHash.search(30))
