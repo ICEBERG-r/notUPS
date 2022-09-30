ed228172646f7e61851a990ee6c59ee4090ce06c
Mark Wilson <69532317+wilsonERR@users.noreply.github.com>
2022-09-30 13:26:30 -0500

    Added Truck class added get_distance function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # Student ID:  010314264
 import csv
 import datetime
+import operator
 
 
 class HashMap:
@@ -61,12 +62,27 @@ class Package:
         return str(self)
 
 
+class Truck:
+    def __init__(self):
+        self.mph = 18.0
+        self.payload = []
+        self.start_time = 8.0
+        self.mileage = 0.0
+        self.has_driver = False
+
+
 class Vertex:
     def __init__(self, loc_id, label):
         self.loc_id = loc_id
         self.label = label
         self.distance = float('inf')
         self.pred_vertex = None
+
+    def __str__(self):
+        return "%s, %s" % (self.loc_id, self.label)
+
+    def __repr__(self):
+        return str(self)
 
 
 class Graph:
@@ -75,7 +91,7 @@ class Graph:
         self.edge_weights = {}
 
     def add_vertex(self, new_vertex):
-        self.adjacency_list[new_vertex]
+        self.adjacency_list[new_vertex] = []
 
     def add_directed_edge(self, from_vertex, to_vertex, weight=1.0):
         self.edge_weights[(from_vertex, to_vertex)] = weight
@@ -104,13 +120,13 @@ def load_packages(filename):
             p = Package(p_id, p_address, p_city, p_state, p_zip, p_deadline, p_weight, p_notes, p_status)
 
             if p.package_id in {1, 2, 13, 14, 15, 16, 20, 21, 27, 34, 35, 39, 40}:
-                truck_1.append(p)
+                truck_1.payload.append(p)
 
             if p.package_id in {3, 5, 6, 7, 8, 12, 18, 25, 26, 29, 30, 31, 32, 36, 37, 38}:
-                truck_2.append(p)
+                truck_2.payload.append(p)
 
             if p.package_id in {4, 9, 10, 11, 17, 19, 22, 23, 24, 28, 33}:
-                truck_3.append(p)
+                truck_3.payload.append(p)
 
             packageHash.insert(p_id, p)
 
@@ -124,7 +140,18 @@ def load_addresses(filename):
 
             address_lookup[loc_address] = loc_id
 
+            v = Vertex(loc_id, loc_address)
+            vertex_array.append(v)
 
+
+def get_distance(loc_1, loc_2):
+    if str(distance_table[loc_1][loc_2]) == '':
+        return distance_table[loc_2][loc_1]
+    else:
+        return distance_table[loc_1][loc_2]
+
+
+'''
 def dijkstra(g, start_vertex):
     unvisited = []
     for current_vertex in g.adjacency_list:
@@ -146,43 +173,65 @@ def dijkstra(g, start_vertex):
             if alt_path_distance < adj_vertex.distance:
                 adj_vertex.distance = alt_path_distance
                 adj_vertex.pred_vertex = current_vertex
+'''
 
-
+'''
 def get_shortest_path(start_vertex, end_vertex):
-    path = ""
+    path = ''
     current_vertex = end_vertex
     while current_vertex is not start_vertex:
-        path = " -> " + str(current_vertex.label) + path
+        path = ' -> ' + str(current_vertex.label) + path
         current_vertex = current_vertex.pred_vertex
     path = start_vertex.label + path
     return path
-
+'''
 
 packageHash = HashMap()
 
-truck_1 = []   # move these inside method?
-truck_2 = []
-truck_3 = []
+truck_1 = Truck()
+truck_2 = Truck()
+truck_3 = Truck()
 
 g = Graph()
 
 address_lookup = {}
+vertex_array = []
+distance_table = list(csv.reader(open('distance.csv', encoding='utf-8-sig')))
+
+
+for n in vertex_array:
+    g.add_vertex(vertex_array[n])
+
+for n in vertex_array:
+    g.add_undirected_edge(vertex_array[n], vertex_array[n+1], float(distance_table[n][n+1]))
 
 load_packages('packages.csv')
 load_addresses('address_vertex.csv')
 
+print(truck_1.payload)
+print(truck_2.payload)
+print(truck_3.payload)
 
-distance_table = list(csv.reader(open('distance.csv')))
-print(float(distance_table[7][3]))
+print(get_distance(0, 4))
+# print(float(distance_table[7][3]))
 
+# print(packageHash.search(30))
 
-print(packageHash.search(30))
+# print(truck_1)
+# print(truck_2)
+# print(truck_3)
 
-print(truck_1)
-print(truck_2)
-print(truck_3)
+# may not implement dijkstra, a greedy algorithm would be easier to implement
+# dijkstra(g, vertex_array[0])
+
+# for v in sorted(g.adjacency_list, key=operator.attrgetter("label")):
+#    if v.pred_vertex is None and v is not vertex_array[0]:
+#        print("HUB to %s: no path exists" % v.label)
+#    else:
+#        print("HUB to %s: %s (total weight: g)" % (v.label, get_shortest_path(vertex_array[0], v), v.distance))
 
 # This is how you can convert a float to a time
-x = 3.6
+# x = 3.6
+# print(str(datetime.timedelta(hours=x))[:-3])
 
-print(str(datetime.timedelta(hours=x))[:-3])
+
